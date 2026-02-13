08f1f7e62fd15eafeb15234222d18adc45e6b071
Olha Kramarenko <okramarenko-ua@singlestore.com>
2026-02-13 16:11:19 +0200

    grab changes for ComplexNavigations tests

--- test/EFCore.SingleStore.FunctionalTests/Query/CaseSensitiveNorthwindQuerySingleStoreFixture.cs
+++ test/EFCore.SingleStore.FunctionalTests/Query/CaseSensitiveNorthwindQuerySingleStoreFixture.cs
@@ -1,21 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using EntityFrameworkCore.SingleStore.FunctionalTests.TestUtilities;
 
 namespace EntityFrameworkCore.SingleStore.FunctionalTests.Query
 {
     public class CaseSensitiveNorthwindQuerySingleStoreFixture<TModelCustomizer> : NorthwindQuerySingleStoreFixture<TModelCustomizer>
-        where TModelCustomizer : IModelCustomizer, new()
+        where TModelCustomizer : ITestModelCustomizer, new()
     {
         protected override string StoreName => "NorthwindCs";
         protected override ITestStoreFactory TestStoreFactory => SingleStoreNorthwindTestStoreFactory.InstanceCs;
-
-        public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
-        {
-            var optionsBuilder = base.AddOptions(builder);
-            new SingleStoreDbContextOptionsBuilder(optionsBuilder).EnableStringComparisonTranslations();
-            return optionsBuilder;
-        }
     }
 }
